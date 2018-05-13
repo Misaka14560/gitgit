212db3529808286e6c8fec91ce3f344166630f80
Misaka14560 <790169235@qq.com>
2018-05-13 16:38:34 +0800

    今天上课的内容

---
+++ gc20180513.py
@@ -0,0 +1,38 @@
+from PIL import Image as Image
+import itchat
+import os
+import math
+itchat.auto_login()
+friends = itchat.get_friends(update=True)[0:]
+user = friends[0]['UserName']
+num = 0
+for i in friends:
+    print('donnloading NO.' + str(num + 1) + " image")
+    img = itchat.get_head_img(userName=i["UserName"])
+    fileImage = open("avatar" + "/" + str(num) + ".jpg", "wb")
+    fileImage.write(img)
+    fileImage.close()
+    num += 1
+
+Is = os.listdir("avatar")
+each_size = int(math.sqrt(float(640 * 640) / len(Is)))
+lines = int(640 / each_size)
+image = Image.new("RGBA", (640, 640))
+
+x = 0
+y = 0
+for i in range(0, len(Is) - 2):
+    try:
+        img = Image.open("avatar" + "/" + str(num) + ".jpg")
+    except IOError:
+        print("error")
+    else:
+        img = img.resize((each_size, each_size), Image.ANTIALIAS)
+        Image.paste(img, (x * each_size, y * each_size))
+        if x == lines:
+            x = 0
+            y += 1
+image = image.convert('RGB')
+image.save("avatar2" + "/" + "all.jpg")
+itchat.send_image("avatar2" + "/" + "all.jpg", "filehelper")
+image.show()
